c79f179d55c0aece98af92123761c49da1e62155
ThorNex <134919917+PluvIIter@users.noreply.github.com>
2025-03-17 16:03:39 +0800

    Update suspend-mods.sh

--- win4-suspend-mods/suspend-mods.sh
+++ win4-suspend-mods/suspend-mods.sh
@@ -2,8 +2,8 @@
 # This file runs during sleep/resume events. 
 # Originally created by ChimeraOS
 
-modprobe -r bmi260_i2c
-modprobe -r bmi260_core
+modprobe -r bmi160_i2c
+modprobe -r bmi160_core
 
 # WM2?
 #echo disabled | tee /sys/bus/i2c/devices/i2c-GXTP7385:00/power/wakeup
